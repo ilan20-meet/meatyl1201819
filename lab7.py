bd6ce0ecf67ad020a7af5a0f66a1c77efb19dda0
ilan20-meet <ilan20@meet.mit.edu>
2018-12-05 12:50:40 -0500

    lab7.2

--- lab7.py
+++ lab7.py
@@ -1,6 +1,6 @@
 from turtle import *
 import random 
-
+tracer(0)
 class Ball(Turtle):
 	def __init__(self, radius, color, x, y, dx,dy):
 		Turtle.__init__(self)
@@ -17,18 +17,33 @@ class Ball(Turtle):
 		oldy = self.ycor()
 		newx = oldx + self.dx
 		newy = oldy + self.dy
+		self.goto(newx,newy)
+		if self.xcor() + self.radius >= 600:
+			self.dx = -self.dx
+		if self.ycor() + self.radius >= 400:
+			self.dy = -self.dy
+		if self.xcor() - self.radius <= -600:
+			self.dx = -self.dx
+		if self.ycor() - self.radius <= -400:
+			self.dy = -self.dy
 def collision(ball1,ball2):
 	radius_sum = ball1.radius + ball2.radius
 	d = ((ball1.xcor() - ball2.xcor())**2 + (ball1.ycor() - ball2.ycor())**2)**0.5
 	if(d < radius_sum):
-		return True
-	else:
-		return False	
+		tempX =  ball1.dx
+		tempY = ball1.dy
+		ball1.dx = ball2.dx
+		ball1.dy = ball2.dy
+		ball2.dx = tempX
+		ball2.dy = tempY
+			
 
-ball1 = Ball(50,"blue", 0, 0, 1, 1)		
-ball2 = Ball(50,"orange", 0, 0, 1, 1)	
-# while True:
-# 	ball1.moveBall()
-# 	ball2.moveBall()
-# 	collision =collision()
+ball1 = Ball(50,"blue", 200, 200, .4, .4)		
+ball2 = Ball(50,"orange", 0, 0, .10, .10)	
+while True:
+	ball1.moveBall()
+	ball2.moveBall()
+	collision(ball1,ball2)
+	update()
+		
 mainloop()
--- recursionlab.py
+++ recursionlab.py
@@ -2,20 +2,20 @@ import turtle
 
 def tree(l):
 	if (l<=10):
-		turtle.backwards(l)
 		turtle.left(20)
+		turtle.backward(l)
 	else:
 		turtle.forward(l)
 		turtle.right(20)
-	tree(l-10)
-	if(l<=10):
-		turtle.backwards(l)
-		turtle.right(20)
+	# tree(l-10)
+	# if(l<=10):
+	# 	turtle.backward(l)
+	# 	turtle.right(20)
 
-	else:
-		turtle.forward(l)
-		turtle.left(20)	
-	tree(l-10)
+	# else:
+	# 	turtle.forward(l)
+	# 	turtle.left(20)	
+	# tree(l-10)
 
 tree(100)					
 	
