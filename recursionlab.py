8a6615c5af7bccfe0cb4816c3ffdbe4774130324
ilan20-meet <ilan20@meet.mit.edu>
2019-01-30 12:54:40 -0500

    :)

--- recursionlab.py
+++ recursionlab.py
@@ -1,15 +1,16 @@
 import turtle
 def tree(l):
-	if (l==0):	
-		turtle.left(10)
-		turtle.backward(l)
-		turtle.left(20)
-		
-	else:	
+	if (l>5):	
 		turtle.forward(l)
 		turtle.right(10)
 		tree(l-5)
-
+		turtle.left(10)
+		turtle.backward(l)
+		turtle.left(20)
+		tree(l-5)
+		turtle.right(20)
+		turtle.forward(l)
+		
 
 tree(50)					
 	
